24aa88e846cfcdc8e89f8538ee6f80cd9c5671b4
Fadly Kayo <f_kayo@yahoo.com>
2017-01-20 10:45:36 +0700

    Update Fri,19.01.16

--- contact_group.js
+++ contact_group.js
@@ -6,35 +6,35 @@ const sqlite3 = require('sqlite3').verbose();
 let file = "address_book.db"
 let db = new sqlite3.Database(file);
 
-class Group {
-  static insertGroup (name) {
-    let INSERT_GROUP = `INSERT INTO groups(name) VALUES('${name}')`
+class ContactGroups {
+  static insertGroup (contact_id, group_id) {
+    let INSERT_GROUP = `INSERT INTO contact_groups(group_id, contact_id) VALUES('${group_id}', '${contact_id}')`
     db.serialize(function() {
       db.run(INSERT_GROUP, function(err) {
         if(err) {
           console.log(err)
         } else {
-          console.log("Insert Data to Groups Success!")
+          console.log("Insert Data to Group_ID Success!")
         }
       })
     })
   }
 
-  static updateGroup (name, id) {
-    let UPDATE_GROUP = `UPDATE groups SET name = '${name}' WHERE id = ${id}`
+  static updateGroup (contact_id, group_id, id) {
+    let UPDATE_GROUP = `UPDATE contact_groups SET group_id = '${group_id}', contact_id = '${contact_id}' WHERE id = ${id}`
     db.serialize(function() {
       db.run(UPDATE_GROUP, function(err) {
         if(err) {
           console.log(err)
         } else {
-          console.log("Update Data Group Success!");
+          console.log("Update Group_ID Success!");
         }
       })
     })
   }
 
   static deleteGroup (id) {
-    let DELETE_GROUP = `DELETE FROM groups WHERE id = ${id}`
+    let DELETE_GROUP = `DELETE FROM contact_groups WHERE id = ${id}`
     db.serialize(function() {
       db.run(DELETE_GROUP, function(err, row) {
         if(err) {
@@ -47,7 +47,7 @@ class Group {
   }
 
   static showGroup () {
-    let SELECT_GROUP = `SELECT * FROM groups`
+    let SELECT_GROUP = `SELECT * FROM contact_groups`
     db.serialize(function() {
       db.each(SELECT_GROUP, function(err, row) {
         if(err) {
@@ -62,10 +62,10 @@ class Group {
 
 let command = repl.start("> ")
 
-command.context.insertGroup = Group.insertGroup;
-command.context.updateGroup = Group.updateGroup;
-command.context.deleteGroup = Group.deleteGroup;
-command.context.showGroup = Group.showGroup;
+command.context.insertGroup = ContactGroups.insertGroup;
+command.context.updateGroup = ContactGroups.updateGroup;
+command.context.deleteGroup = ContactGroups.deleteGroup;
+command.context.showGroup = ContactGroups.showGroup;
 
 
 /* Driver Code
